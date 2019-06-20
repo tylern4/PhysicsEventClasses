c252b8ae78d01961ed70fc1e787fe867acfe0fbe
Nick Tyler <tylern@canisius.edu>
2019-06-20 10:34:52 -0400

    Adding comments

--- src/main.cpp
+++ src/main.cpp
@@ -6,27 +6,50 @@
 #define NUM_THREADS 4
 
 int main(int argc, char **argv) {
+  // Need this to make sure root doesn't break
   ROOT::EnableThreadSafety();
+
+  // Start timer
   auto start = std::chrono::high_resolution_clock::now();
+
+  // Make a vector of vectors of strings the size of the number of threads
   std::vector<std::vector<std::string>> infilenames(NUM_THREADS);
+  // Get the output file name
   std::string outfilename;
 
   if (argc >= 2) {
+    // First argument is the output file
     outfilename = argv[1];
+    // All other files are split evently by the under of threads
     for (int i = 2; i < argc; i++) infilenames[i % NUM_THREADS].push_back(argv[i]);
   } else {
     return 1;
   }
 
-  std::future<size_t> t[NUM_THREADS];
+  // Make a set of threads (Futures are special threads which return a value)
+  std::future<size_t> threads[NUM_THREADS];
+
+  // Define events to be used to get Hz later
   size_t events = 0;
+
+  // Make your histograms object as a shared pointer that all the threads will have
   auto hists = std::make_shared<Histogram>(outfilename);
+
+  // For each thread
   for (size_t i = 0; i < NUM_THREADS; i++) {
-    t[i] = std::async(run_files, infilenames.at(i), hists, i);
+    // Set the thread to run a task A-Syncroisly
+    // The function we run is the first argument (run_files)
+    // The functions areruments are all the remaining arguments
+    threads[i] = std::async(run_files, infilenames.at(i), hists, i);
   }
+
+  // For each thread
   for (size_t i = 0; i < NUM_THREADS; i++) {
-    events += t[i].get();
+    // Get the information from the thread in this case how many events each thread actually computed
+    events += threads[i].get();
   }
+
+  // Timer and Hz calculator functions that print at the end
   std::cout.imbue(std::locale(""));  // Puts commas in
   std::chrono::duration<double> elapsed_full = (std::chrono::high_resolution_clock::now() - start);
   std::cout << RED << elapsed_full.count() << " sec" << DEF << std::endl;
--- src/main.hpp
+++ src/main.hpp
@@ -16,18 +16,26 @@
 #include "reaction.hpp"
 
 size_t run(std::shared_ptr<TChain> _chain, std::shared_ptr<Histogram> _hists, int thread_id) {
+  // Get the number of events in this thread
   size_t num_of_events = (int)_chain->GetEntries();
-
+  // Print some information for each thread
   std::cout << "=============== " << RED << "Thread " << thread_id << DEF << " =============== " << BLUE
             << num_of_events << " Events " << DEF << "===============\n";
+
+  // Make a data object which all the branches can be accessed from
   auto data = std::make_shared<Branches6>(_chain);
 
+  // Total number of events "Processed"
   size_t total = 0;
+  // For each event
   for (size_t current_event = 0; current_event < num_of_events; current_event++) {
+    // Get current event
     _chain->GetEntry(current_event);
+    // If we are the 0th thread print the progress of the thread every 1000 events
     if (thread_id == 0 && current_event % 1000 == 0)
       std::cout << "\t" << (100 * current_event / num_of_events) << " %\r" << std::flush;
 
+    // Quick electron cuts
     bool elec = true;
     elec &= (data->q(0) == NEGATIVE);
     elec &= (data->ec(0) > 0);
@@ -37,14 +45,17 @@ size_t run(std::shared_ptr<TChain> _chain, std::shared_ptr<Histogram> _hists, in
     elec &= (data->stat(0) > 0);
     elec &= (data->sc(0) > 0);
     elec &= (data->dc(0) > 0);
-    elec &= (data->nphe(0) > 30);
     elec &= (data->dc_stat(0) > 0);
     if (!elec) continue;
 
+    // If we pass electron cuts the event is processed
     total++;
 
+    // Make a reaction class from the data given
     auto event = std::make_unique<Reaction>(data);
+    // For each particle in the event
     for (int part = 1; part < data->gpart(); part++) {
+      // Check particle ID's and fill the reaction class
       if (data->id(part) == PIP) {
         event->SetPip(part);
       } else if (data->id(part) == PROTON) {
@@ -56,15 +67,22 @@ size_t run(std::shared_ptr<TChain> _chain, std::shared_ptr<Histogram> _hists, in
       }
     }
 
-    if (event->SingleP()) _hists->WvsQ2_Fill(event->W(), event->Q2(), data->dc_sect(0));
+    // Check the reaction class what kind of even it is and fill the appropriate histograms
+    if (event->SinglePip()) _hists->WvsQ2_Fill(event->W(), event->Q2(), data->dc_sect(0));
   }
 
+  // Return the total number of events
   return total;
 }
 
 size_t run_files(std::vector<std::string> inputs, std::shared_ptr<Histogram> hists, int thread_id) {
+  // Called once for each thread
+  // Make a new chain to process for this thread
   auto chain = std::make_shared<TChain>("h10");
+  // Add every file to the chain
   for (auto in : inputs) chain->Add(in.c_str());
+
+  // Run the function over each thread
   return run(chain, hists, thread_id);
 }
 
